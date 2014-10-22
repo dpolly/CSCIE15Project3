5e5269d44ff5d723aa660fa16e8bb6d3b32d707a
Denise Polly <dpolly@g.harvard.edu>
2014-10-22 11:12:26 -0500

    Uploading to Test Views on DO

--- app/views/master.blade.php
+++ app/views/master.blade.php
@@ -1,14 +1,39 @@
 <!DOCTYPE html>
-<html>
-<head>
 
-    <title>@yield('title', 'Dpolly.me')</title>
+<html class="no-js" lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>@yield('title', 'Dpolly.me')</title>
+        <link rel="stylesheet" href='{{ asset('css/foundation.css') }}' />
+        <link rel="stylesheet" href='{{ asset('css/app.css') }}' />
+     </head>
 
-</head>
-<body>
-
-    <h1>You Made it to the Master</h1>
-    @yield('content')
-
-</body>
+    <body>
+        <div id="header" class="row">
+            <div class="small-3 columns">
+                <img src='{{ asset('img/logo.png') }}'alt="DMP logo">
+            </div>
+            <div class="panel">
+                    <h1>Web Development Tools</h1>
+            </div>
+        </div>
+        <div id="content" class="row">
+            <div class="small-4 columns">
+                <a href="text" alt="Text">Generate Random Text </a>
+                <a href="users" alt="Users">Generate Random Users</a>
+            </div>
+            <div class="small-8 columns">
+                <div class="panel"
+                    @yield('content')
+                </div>
+            </div>
+        </div>
+        <div id="footer" class="row">
+            <div class="panel" >
+                <h6>CSCIE-15 Dynamic Web Applications Project 3</h6>
+            </div>
+        </div>
+        </div>
+    </body>
 </html>
