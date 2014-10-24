e610c9d66bd83d9c94e62c7c86918638ff7a6a18
Denise Polly <dpolly@g.harvard.edu>
2014-10-23 22:55:34 -0500

    Fixed link

--- app/views/tools.blade.php
+++ app/views/tools.blade.php
@@ -9,7 +9,7 @@
     <div class="panel">
         <a class="links" href="http://www.csszengarden.com/216//">Link to CSS Zen Garden</a><br>
         <a class="links" href="http://jquery.com/">Link to JQuery</a><br>
-        <a class="links" href="http://php.net/"></a>Link to PHP<br>
+        <a class="links" href="http://php.net/">Link to PHP</a><br>
         <a class="links" href="http://www.w3.org/">Link to W3C</a><br>
         <a class="links" href="http://git-scm.com/book">Link to Git</a><br>
         <a class="links" href="https://github.com/">Link to Github</a>
