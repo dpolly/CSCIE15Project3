d4335afef68d2e9feccd222d772a0a7ee38f978c
Denise Polly <dpolly@g.harvard.edu>
2014-10-23 21:09:34 -0500

    minor cleanup

--- app/views/text.blade.php
+++ app/views/text.blade.php
@@ -5,15 +5,13 @@
  @stop
 
  @section('content form')
-
     <div class="panel">
-         <h4>Number of Paragraphs</h4>
-         {{ Form::open(array('url' => '/text', 'method' => 'POST')) }}
-            {{ Form::input('text', 'paragraph') }}
-            {{ Form::submit('Generate'); }}
-          {{ Form::close() }}
+        <h4>Number of Paragraphs</h4>
+           {{ Form::open(array('url' => '/text', 'method' => 'POST')) }}
+           {{ Form::input('text', 'paragraph') }}
+           {{ Form::submit('Generate'); }}
+           {{ Form::close() }}
     </div>
-
  @stop
 
 
