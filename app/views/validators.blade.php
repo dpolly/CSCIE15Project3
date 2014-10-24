882b8c6d9a8b9ce1377cd3eae61e402a3690ac42
Denise Polly <dpolly@g.harvard.edu>
2014-10-23 21:31:48 -0500

    Minor fixes

--- app/views/validators.blade.php
+++ app/views/validators.blade.php
@@ -7,6 +7,6 @@
  @section('content')
     <div class="panel">
         <a class="links" href="http://validator.w3.org/">Link to W3C Markup Validator</a><br>
-        <a class="links" href="http://jigsaw.w3.org/css-validator/">Lik to W3C CSS Validator</a><br>
+        <a class="links" href="http://jigsaw.w3.org/css-validator/">Link to W3C CSS Validator</a><br>
     </div>
  @stop
