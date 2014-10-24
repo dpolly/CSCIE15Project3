3cfa52c8fc93c0263954adfa381584e631a30950
Denise Polly <dpolly@g.harvard.edu>
2014-10-23 21:10:33 -0500

    Initial Load

---
+++ app/views/validators.blade.php
@@ -0,0 +1,12 @@
+ @extends('master')
+
+ @section('title')
+        Dpolly.me Project3 Validators
+ @stop
+
+ @section('content')
+    <div class="panel">
+        <a class="links" href="http://validator.w3.org/">Link to W3C Markup Validator</a><br>
+        <a class="links" href="http://jigsaw.w3.org/css-validator/">Lik to W3C CSS Validator</a><br>
+    </div>
+ @stop
